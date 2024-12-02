f7de261815ff0b39da09d0cefc9d05c2132c7e2d
Mahsa <mahsaadlooo@gmail.com>
2024-12-02 11:28:43 -0800

    feat: :art: Use context instead of props in all parts of the project.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,9 +2,9 @@ import { useContext } from "react";
 
 import { CartContext } from "../store/Shopping-cart-context.jsx";
 
-export default function Cart({ onUpdateItemQuantity }) {
+export default function Cart() {
   // const cartCtx = useContext(CartContext);
-  const { items } = useContext(CartContext);
+  const { items, updateItemQuantity } = useContext(CartContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -27,11 +27,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
